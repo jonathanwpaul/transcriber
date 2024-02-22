85ab98d38dac75227bbb67e506153a1ac7c9c81f
fabernex <paulwjonathan@gmail.com>
2024-02-21 23:47:32 -0500

    add Dialog component

---
+++ src/components/Dialog.jsx
@@ -0,0 +1,38 @@
+import {
+  DialogActions,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemButton,
+  Dialog as MuiDialog,
+} from '@mui/material'
+
+export const Dialog = ({
+  open,
+  onClose,
+  selectedValue,
+  items,
+  ...otherProps
+}) => {
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
+
+  const handleListItemClick = value => {
+    onClose(value)
+  }
+
+  console.log(items)
+  return (
+    <MuiDialog onClose={handleClose} open={open} {...otherProps}>
+      <DialogTitle>Saved entries:</DialogTitle>
+      <List>
+        {items.map(item => (
+          <ListItem onClick={() => handleListItemClick(item)}>
+            <ListItemButton>{item}</ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </MuiDialog>
+  )
+}
