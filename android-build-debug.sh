ccb51c26c4a80202ef34cac779fac9d13d226c76
fabernex <paulwjonathan@gmail.com>
2024-09-07 14:07:52 -0400

    fix pathing, add script reference in android.yml

--- android-build-debug.sh
+++ android-build-debug.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# if android hasn't been added to project yet
 npx cap add android 2>/dev/null
 npm run build
 npx cap sync
-cd android && ./gradlew assembleDebug
-cd .. && mkdir -p "dist" && cp android/app/build/outputs/apk/debug/app-debug.apk dist/app-debug.apk
-echo "wrote debug apk to dist/app-debug.apk"
