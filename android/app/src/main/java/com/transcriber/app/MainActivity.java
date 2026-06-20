41a73fc10aa1af138995c1e4da1298c33a92f150
Jonathan <paulwjonathan@gmail.com>
2026-06-20 18:03:01 -0400

    debug release app

--- android/app/src/main/java/com/transcriber/app/MainActivity.java
+++ android/app/src/main/java/com/transcriber/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.transcriber.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void start() {
+        WebView.setWebContentsDebuggingEnabled(true);
+        super.start();
+    }
+}
